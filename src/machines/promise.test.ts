f44c1500a9d943c6672f4baca875b093ce9f6533
laj <larry.jones@reveal-energy.com>
2022-08-28 09:57:12 -0500

    Add events and transitions

--- src/machines/promise.test.ts
+++ src/machines/promise.test.ts
@@ -1,3 +1,4 @@
+import { interpret } from 'xstate';
 import promiseMachine from './promise';
 import { D } from "@mobily/ts-belt";
 
@@ -17,5 +18,19 @@ describe('newly created promiseMachine', () => {
 
     it('has three states: "pending", "resolved", "rejected"', () => {
         expect(D.keys(promiseMachine.states)).toEqual(['pending', 'resolved', 'rejected']);
+    });
+
+    it('specifies events that cause transitions from one state to another', () => {
+        expect(D.keys(promiseMachine.states.pending.on)).toEqual(['RESOLVE', 'REJECT']);
+    });
+
+    it.each([
+        { on: 'RESOLVE', to: { target: 'resolved' } },
+        { on: 'REJECT', to: { target: 'rejected' } },
+    ])('Transitions to $to on $on', ({ on, to }) => {
+        const promiseService = interpret(promiseMachine);
+        promiseService.start();
+        const nextState = promiseService.send({ type: on });
+        expect(nextState.value).toEqual(to.target);
     });
 });
--- src/machines/promise.tsx
+++ src/machines/promise.tsx
@@ -2,10 +2,16 @@ import { createMachine } from 'xstate';
 
 const promiseMachine = createMachine(
     {
+        predictableActionArguments: true,  // Recommended for V5 compatibility
         id: 'promise',
         initial: 'pending',
         states: {
-            pending: {},
+            pending: {
+                on: {
+                    'RESOLVE': { target: 'resolved' },
+                    'REJECT': { target: 'rejected' },
+                },
+            },
             resolved: {},
             rejected: {},
         },
